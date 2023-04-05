2c2572632635e53dce0cbe7a2fbe7bc930a4a57c
nimeshaMadubashini <nimeshamadubashini040@gmail.com>
2023-04-05 14:04:33 +0530

    add regex

--- src/lk/ijse/hibernate/controller/UserEditFormController.java
+++ src/lk/ijse/hibernate/controller/UserEditFormController.java
@@ -6,9 +6,11 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Paint;
 import lk.ijse.hibernate.bo.BOFactory;
 import lk.ijse.hibernate.bo.custome.UsrEditBO;
 import lk.ijse.hibernate.dto.Notification;
@@ -16,6 +18,7 @@ import lk.ijse.hibernate.dto.UserDto;
 import lk.ijse.hibernate.utill.nave.Navigation;
 import lk.ijse.hibernate.utill.nave.Routes;
 
+import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -71,7 +74,7 @@ public class UserEditFormController {
         passWordMatcher = passwordPattern.matcher(txtPassword.getText());
 
         Pattern confirmPasswordPattern = Pattern.compile("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8}$");
-        confirmPsMatcher = passwordPattern.matcher(conformPassword.getText());
+        confirmPsMatcher = confirmPasswordPattern.matcher(conformPassword.getText());
     }
 
     public void initialize() {
@@ -81,8 +84,8 @@ public class UserEditFormController {
     }
 
     @FXML
-    void addSudentOnAction(ActionEvent event) {
-
+    void addSudentOnAction(ActionEvent event) throws IOException {
+Navigation.navigation(Routes.STUDENT,pane);
     }
 
     String password;
@@ -96,21 +99,46 @@ public class UserEditFormController {
         String conpassword = conformPassword.getText();
         UserDto dto = new UserDto(nic, username, password);
         try {
-            boolean isUpdate = usrEditBO.updateUser(dto);
-            if (isUpdate) {
+            if (nicMatcher.matches()) {
+                    if (userNameMatcher.matches()) {
+                        if (passWordMatcher.matches()) {
+                            if (conpassword.equals(password) && confirmPsMatcher.matches()) {
+                                boolean isUpdate = usrEditBO.updateUser(dto);
+                                if (isUpdate) {
 
-                String url = "/lk/ijse/hibernate/assest/icons8-check-mark-48.png";
-                String title = "Update Successful";
-                String text = "Hey, Update Successful";
-                Notification.showNotification(url, title, text);
+                                    String url = "/lk/ijse/hibernate/assest/icons8-check-mark-48.png";
+                                    String title = "Update Successful";
+                                    String text = "Hey, Update Successful";
+                                    Notification.showNotification(url, title, text);
 
-            }
-            String url = "/lk/ijse/hibernate/assest/icons8-select-no-64 (1).png";
-            String title = "Update UnSuccessful";
-            String text = "Incorrect Nic";
-            Notification.showNotification(url, title, text);
+                                }else{
+                                String url = "/lk/ijse/hibernate/assest/icons8-select-no-64 (1).png";
+                                String title = "Update UnSuccessful";
+                                String text = "Incorrect Nic";
+                                Notification.showNotification(url, title, text);
+                            }
+                            } else {
+                                conformPassword.requestFocus();
+                                conformPassword.setFocusColor(Paint.valueOf("red"));
+                                llconfirmpsw.setText("Not Match password");
+                            }
+                        } else {
+                            txtPassword.requestFocus();
+                            txtPassword.setFocusColor(Paint.valueOf("Red"));
+                            lblPassword.setText("invalid password");
+                        }
+                    } else {
+                        txtUserName.requestFocus();
+                        txtUserName.setFocusColor(Paint.valueOf("Red"));
+                        lblusername.setText("invalid user name");
+                    }
+        } else {
+            txtNic.requestFocus();
+            txtNic.setFocusColor(Paint.valueOf("Red"));
+            lblnic.setText("invalid staff id");
+        }
 
-        } catch (Exception ex) {
+    } catch (Exception ex) {
             throw new RuntimeException(ex);
         }
 
@@ -119,57 +147,113 @@ public class UserEditFormController {
 
     @FXML
     void cloeEyeOnAction(MouseEvent event) {
-
+        txtPassword1.setVisible(true);
+        openEye.setVisible(true);
+        closeEye.setVisible(false);
+        txtPassword.setVisible(false);
     }
 
     @FXML
     void hidePasswordOnKeyRelease(KeyEvent event) {
+        lblPassword.setText("");
+        txtPassword.setFocusColor(Paint.valueOf("Blue"));
+        Pattern passwordPattern = Pattern.compile("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8}$");
+        passWordMatcher = passwordPattern.matcher(txtPassword.getText());
 
+        if (!passWordMatcher.matches()) {
+            txtPassword.requestFocus();
+            txtPassword.setFocusColor(Paint.valueOf("Red"));
+            lblPassword.setText("invalid password");
+        }
+        password=txtPassword.getText();
+        txtPassword1.setText(password);
+
+    }
+@FXML
+void confirmpswONKeyRelease(KeyEvent keyEvent){
+    llconfirmpsw.setText("");
+    conformPassword.setFocusColor(Paint.valueOf("Blue"));
+    Pattern confirmPasswordPattern = Pattern.compile("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8}$");
+    confirmPsMatcher = confirmPasswordPattern.matcher(conformPassword.getText());
+
+    if (!confirmPsMatcher.matches()) {
+        conformPassword.requestFocus();
+        conformPassword.setFocusColor(Paint.valueOf("Red"));
+        llconfirmpsw.setText("not match password");
+    }
+
+}
+    @FXML
+    void homeOnAtion(ActionEvent event) throws IOException {
+Navigation.navigation(Routes.HOME,pane);
     }
 
     @FXML
-    void homeOnAtion(ActionEvent event) {
-
-    }
-
-    @FXML
-    void keyOnAction(ActionEvent event) {
-
+    void keyOnAction(ActionEvent event) throws IOException {
+Navigation.navigation(Routes.ROOM,pane);
     }
 
     @FXML
     void openEyeOnAction(MouseEvent event) {
-
+        txtPassword1.setVisible(false);
+        openEye.setVisible(false);
+        closeEye.setVisible(true);
+        txtPassword.setVisible(true);
     }
 
     @FXML
-    void roomOnAction(ActionEvent event) {
-
+    void roomOnAction(ActionEvent event) throws IOException {
+Navigation.navigation(Routes.ROOM,pane);
     }
 
     @FXML
     void showPasswordOnKeyRelease(KeyEvent event) {
-
+        password=txtPassword1.getText();
+        txtPassword.setText(password);
     }
 
     @FXML
     void txtNicOnKeyReleased(KeyEvent event) {
+        lblnic.setText("");
+        txtNic.setFocusColor(Paint.valueOf("Blue"));
+        Pattern idPattern = Pattern.compile("^[0-9]{9}[vVxX]|[0-9]{12}[vVxX]|[0-9]{12}|[0-9]{9}$");
+        nicMatcher = idPattern.matcher(txtNic.getText());
 
+
+        if (!nicMatcher.matches()) {
+            txtNic.requestFocus();
+            txtNic.setFocusColor(Paint.valueOf("Red"));
+            lblnic.setText("invalid nic");
+        }
     }
 
     @FXML
     void txtNicOnkeyPressed(KeyEvent event) {
-
+        if (event.getCode().equals(KeyCode.ENTER)) {
+            txtUserName.requestFocus();
+        }
     }
 
     @FXML
     void txtUserNameOnKeyPress(KeyEvent event) {
-
+        if (event.getCode().equals(KeyCode.ENTER)) {
+            txtPassword.requestFocus();
+        }
     }
 
     @FXML
     void txtUserNameOnKeyRelease(KeyEvent event) {
+        lblusername.setText("");
+        txtUserName.setFocusColor(Paint.valueOf("Blue"));
+        Pattern userNamePattern = Pattern.compile("^[a-zA-Z0-9]{4,}$");
+        userNameMatcher = userNamePattern.matcher(txtUserName.getText());
 
+
+        if (!userNameMatcher.matches()) {
+            txtUserName.requestFocus();
+            txtUserName.setFocusColor(Paint.valueOf("Red"));
+            lblusername.setText("invalid user name");
+        }
     }
 
 }
